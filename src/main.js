f3846edcbf8febed5aadd990dc64b3e004ad4e75
Robert Chapman <chapmanrobert12@gmail.com>
2024-08-14 11:41:29 -0500

    Added javascript to get content from the rust backend and manage them in a list

--- src/main.js
+++ src/main.js
@@ -1,18 +1,99 @@
+
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
+let clipboardHistory = [];
+let filteredHistory = [];
+let selectedIndex = null;
+document
+  .getElementById("search-bar")
+  .addEventListener("input", filterClipboardHistory);
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+document
+  .getElementById("copy-button")
+  .addEventListener("click", copyToSystemClipboard);
+
+async function fetchClipboardHistory() {
+  const newHistory = await window.__TAURI__.invoke("get_clipboard_history");
+
+  if (JSON.stringify(newHistory) !== JSON.stringify(clipboardHistory)) {
+    clipboardHistory = newHistory;
+    filterClipboardHistory();
+  }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+function updateClipboardHistoryUI(history) {
+  const historyElement = document.getElementById("clipboard-history");
+
+  historyElement.innerHTML = "";
+  history.forEach((item, index) => {
+    const div = document.createElement("div");
+    div.classList.add("line");
+
+    const p = document.createElement("p");
+    div.appendChild(p);
+
+    let img = document.createElement("img");
+    img.src = "assets/x.svg";
+    img.alt = "Delete Button";
+    img.onclick = () => deleteItem(item);
+    div.appendChild(img);
+
+    p.classList.add("item");
+    p.innerText = item;
+
+    if (index === selectedIndex) {
+      div.classList.add("selected");
+    }
+
+    p.onclick = () => {
+      showPreview(item);
+      highlightSelectedItem(index);
+    };
+    historyElement.appendChild(div);
   });
-});
+}
+
+function showPreview(content) {
+  let previewElement = document.getElementById("current-clip");
+  previewElement.textContent = content;
+}
+
+async function deleteItem(content) {
+  let previewElement = document.getElementById("current-clip");
+  if (content == previewElement.textContent) {
+    previewElement.textContent = "";
+  }
+
+  await invoke("delete_item", { item: content });
+}
+
+async function copyToSystemClipboard() {
+  const previewElementContent =
+    document.getElementById("current-clip").textContent;
+  if (previewElementContent !== "") {
+    await invoke("copy_to_system", { content: previewElementContent });
+  }
+}
+
+function highlightSelectedItem(index) {
+  selectedIndex = index;
+  updateClipboardHistoryUI(filteredHistory);
+}
+
+function filterClipboardHistory() {
+  const searchTerm = document.getElementById("search-bar").value.toLowerCase();
+  filteredHistory = clipboardHistory.filter((item) =>
+    item.toLowerCase().includes(searchTerm)
+  );
+  const previewElement = document.getElementById("current-clip");
+  filteredHistory.forEach((line, index) => {
+    if (line == previewElement.textContent) {
+      selectedIndex = index;
+    }
+  });
+  updateClipboardHistoryUI(filteredHistory);
+}
+
+setInterval(fetchClipboardHistory, 1000);
+
+filteredHistory = clipboardHistory;
